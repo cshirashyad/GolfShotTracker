cc44e6574437b776e3fb1f91e344a59788101e3b
Chandra Shirashyad <chaand@gmail.com>
2026-01-03 14:20:27 -0800

    Initial Commit

---
+++ GolfShotTracker/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  GolfShotTracker
+//
+//  Created by Chandra on 1/3/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ GolfShotTrackerTests/GolfShotTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfShotTrackerTests.swift
+//  GolfShotTrackerTests
+//
+//  Created by Chandra on 1/3/26.
+//
+
+import Testing
+@testable import GolfShotTracker
+
+struct GolfShotTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
